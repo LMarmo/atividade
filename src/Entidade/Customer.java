878d168d914e6066de0b6431a8f0bc346f4b50cc
Lucas <lukasmarmo@gmail.com>
2019-11-11 22:37:08 -0300

    Move Method

--- src/Entidade/Customer.java
+++ src/Entidade/Customer.java
@@ -37,7 +37,7 @@ class Customer {
         Rental each = (Rental) rentals.nextElement();
         
         
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -58,27 +58,6 @@ class Customer {
      return result;
      }
 
-    private double amountFor(Rental aRental) {
-        //determine amounts for each line
-        double thisAmount = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2)
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
 
   
 }
