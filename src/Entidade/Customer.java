94b3983ff4e76cac149ab45b8edd763e6aa71ba2
Lucas <lukasmarmo@gmail.com>
2019-11-13 19:57:13 -0300

    Replace Temp with Query

--- src/Entidade/Customer.java
+++ src/Entidade/Customer.java
@@ -33,11 +33,11 @@ class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
+       
         Rental each = (Rental) rentals.nextElement();
         
         
-            thisAmount = each.getCharge();
+             each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -47,8 +47,8 @@ class Customer {
 
         //show figures for this rental
        result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
      //add footer lines
