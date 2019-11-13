a5db12948fe91ee2d8a04a5b1a65218c23c2d52d
Lucas <lukasmarmo@gmail.com>
2019-11-13 20:11:28 -0300

    Extract Method

--- src/Entidade/Customer.java
+++ src/Entidade/Customer.java
@@ -35,15 +35,10 @@ class Customer {
      while (rentals.hasMoreElements()) {
        
         Rental each = (Rental) rentals.nextElement();
-        
-        
-             each.getCharge();
+            
+            
 
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
+       frequentRenterPoints += each.getFrequentRenterPoints();
 
         //show figures for this rental
        result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -57,6 +52,15 @@ class Customer {
              " frequent renter points";
      return result;
      }
+
+    private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+        // add frequent renter points
+        frequentRenterPoints ++;
+        // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                each.getDaysRented() > 1) frequentRenterPoints ++;
+        return frequentRenterPoints;
+    }
 
 
   
